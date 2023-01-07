cf6a319a15af081f55654899461f961928fe2dad
Mariya Batanova <batanova708@gmail.com>
2023-01-07 20:45:02 +0200

    Add Task1

---
+++ index.js
@@ -0,0 +1,19 @@
+//Mariia Batanova
+
+//Task 1
+//написати функцію,
+//яка повертає слово 'odd' або 'even', в залежності від прийнятого аргументу(число).
+//Значення для перевірки функції вводить користувач.
+
+// const number = prompt("Enter any number:");
+
+// const isEvenOrOddNumber = function (number) {
+//   if (number % 2 === 0) {
+//     return "Even";
+//   }
+//   return "Odd";
+// };
+
+// console.log(isEvenOrOddNumber(number));
+
+//Tast 2
